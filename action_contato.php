91b5ee66ed268640286a5169d3f0272db3a87862
borbaallex <borbaallex@gmail.com>
2025-10-06 15:13:57 -0300

    Update action_contato.php

--- action_contato.php
+++ action_contato.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Locadora IFPR - Resultado do Formulário</title>
+    <title>Informações IFPR - Resultado do Formulário</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -12,7 +12,7 @@
 
     <!-- Cabeçalho -->
     <header>
-        <h2>Locadora IFPR</h2>
+        <h2>Informações IFPR</h2>
         <nav>
             <a href="index.html">Início</a>
             <a href="carros.html">Carros</a>
